c15a24e5e4198e72d6d7599377ff70b35ee1e182
Owner <129130014+hysteria222@users.noreply.github.com>
2024-09-23 18:20:50 -0500

    iforgottocommit handling put for projects

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,26 @@
 // add middlewares here related to projects
+const Projects = require('./projects-model0')
+
+async function validateProjectId(req, res, next) {
+    try {
+      const projects = await Projects.get(req.params.id)
+      if (!projects) {
+        res.status(404).json({
+          message: "projects not found",
+        })
+      } else {
+        req.projects = projects 
+        next()
+      }
+    } catch (err) {
+      res.status(500).json({
+        message: "problem finding projects",
+      })
+    }
+  }
+
+
+  
+module.exports = {
+    validateProjectId,
+  }
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,56 @@
 // Write your "projects" router here!
+const express = require('express');
+const router = express.Router();
+
+const Projects = require('./projects-model');
+
+router.get('/', (req, res, next) => {
+  Projects.get()
+    .then((projects) => {
+      res.json(projects);
+      console.log(projects);
+    })
+    .catch(next);
+});
+
+router.get('/:id', (req, res, next) => {
+  const { id } = req.params.id;
+  Projects.get(id)
+    .then((project) => {
+      res.json(project);
+    })
+    .catch(next);
+});
+
+router.post('/', async (req, res, next) => {
+  const project = {
+    name: req.body.name,
+    description: req.body.description,
+  };
+  if (!project.name || !project.description) {
+    res.status(400).json({
+      message: 'Missing required fields.',
+    });
+  }
+  try {
+    const result = await Projects.insert(project);
+    res.status(201).json(result);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.put('/', (req, res, next) => {});
+router.delete('/', (req, res, next) => {});
+router.get('/', (req, res, next) => {});
+
+router.use((err, req, res, next) => {
+  //eslint-disable-line
+  res.status(err.status || 500).json({
+    customMessage: 'womp x2',
+    message: err.message,
+    stack: err.stack,
+  });
+});
+
+module.exports = router;
